16cf35f7532111e6bf4aa6c3b2fc285fc878d72e
Robert Yeakel <robertyeakeltroidl@gmail.com>
2021-04-24 16:42:08 -0700

    add breakpoints to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,12 @@ module.exports = {
             primary: '#144EA5',
             'primary-dark': '#121F3E',
         },
+        screens: {
+            sm: { min: '0px', max: '767px' },
+            md: { min: '768px', max: '1023px' },
+            lg: { min: '1024px', max: '1279px' },
+            xl: { min: '1280px', max: '1535px' },
+        },
     },
     variants: {
         extend: {},
